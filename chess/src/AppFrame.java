2a93ec9b6d143d57b7809c349f6fb3546045c42d
ASUS <ASUS@DESKTOP-OVHSBR3.workgroup>
2017-04-01 10:42:12 +0800

    五子棋

---
+++ chess/src/AppFrame.java
@@ -0,0 +1,16 @@
+
+
+import javax.swing.JFrame;
+
+public class AppFrame extends JFrame {
+
+	public AppFrame(){
+		setTitle("Îå×ÓÆå");
+		setSize(500, 500);
+		setLocationRelativeTo(null);
+		setResizable(false);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setVisible(true);
+	
+	}
+}
